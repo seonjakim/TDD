d401cba54dc851c19872152fc30d04fe44879d47
seonjakim <seonja.kim0@gmail.com>
2020-08-23 12:55:18 +0900

    post reducer with test

---
+++ src/reducers/posts/spec.js
@@ -0,0 +1,29 @@
+import { types } from "../../actions/types";
+import postsReducer from "./reducer";
+
+describe("Posts Red", () => {
+  it("should return default state", () => {
+    const newState = postsReducer(undefined, {});
+    expect(newState).toEqual([]);
+  });
+
+  it("should return new state if receiving type", () => {
+    const posts = [
+      {
+        title: "test 1",
+      },
+      {
+        title: "test 2",
+      },
+      {
+        title: "test 3",
+      },
+    ];
+
+    const newState = postsReducer(undefined, {
+      type: types.GET_POSTS,
+      payload: posts,
+    });
+    expect(newState).toEqual(posts);
+  });
+});
